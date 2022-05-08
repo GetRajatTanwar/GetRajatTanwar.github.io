549690790b9948c3c60f787db972c9e79eb1a600
Rajat Tanwar <getrajattanwar@gmail.com>
2022-05-08 23:15:54 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -93,7 +93,7 @@ function NavBar() {
 
             <Nav.Item className="fork-btn">
               <Button
-                href="https://github.com/GetRajatTanwar/RajatTanwarPortfolio"
+                href="https://github.com/GetRajatTanwar/GetRajatTanwar.github.io"
                 target="_blank"
                 className="fork-btn-inner"
               >
